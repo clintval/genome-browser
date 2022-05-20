771f07a335dc7499298fdc96d77164c267973c6c
Anna Julia Engel anna.julia.engel@gmail.com <anna.julia.engel@gmail.com>
2022-05-20 14:41:03 +0200

    Update comments in demo.py

--- scripts/demo.py
+++ scripts/demo.py
@@ -14,10 +14,9 @@ graph_track.add_graph(
 )
 g.add_track(graph_track)
 
-# Plot 9 random interval features (random start, length, orientation, and color).
+# Plot 9 random intervals.
 interval_track = gb.IntervalTrack("Random Intervals", height_ratio=0.4, step=50)
 for _ in range(9):
-    # Feature must follow iterable as (position, width, strand, color)
     interval = gb.Interval(
         chrom="chr3",
         start=np.random.randint(0, n),
